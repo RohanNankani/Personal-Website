7d79975f42a60bf2f6243f3de95e1e025e7b808c
Rohan Nankani <rohan.nankani2021@gmail.com>
2024-01-28 00:19:52 -0500

    added last name

--- src/components/content/AboutMeContent.jsx
+++ src/components/content/AboutMeContent.jsx
@@ -4,7 +4,7 @@ const AboutMeContent = () => {
         <div>
             <h1 style={{marginBottom: '20px'}}>Hello world! 👋</h1> 
             <p>
-                I am Rohan, a first-year Software Engineering student @ UofWaterloo <br/>
+                I am Rohan Nankani, a first-year Software Engineering student @ UofWaterloo <br/>
                 <br/>
                 I like full-stack engineering with interest in AI/ML/DL. <br/>
                 <br/>
